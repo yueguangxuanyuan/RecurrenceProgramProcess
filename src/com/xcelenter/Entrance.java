16d8bab69b6b812b8983a6720c42f7fe78f922e0
Youhua Wu <yueguangxuanyuan@gmail.com>
2019-01-06 16:33:56 +0800

    将文件路径写入配置文件

--- src/com/xcelenter/Entrance.java
+++ src/com/xcelenter/Entrance.java
@@ -2,11 +2,26 @@ package com.xcelenter;
 
 import com.xcelenter.Bussiness.RecurrenceProgramProcess;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class Entrance {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = new FileInputStream("resource/config.properties");
+
+        Properties properties = new Properties();
+        properties.load(inputStream);
+
+        String sourceDir = properties.getProperty("sourceDir");
+        String targetDir = properties.getProperty("targetDir");
+        String tmpDir = properties.getProperty("tmpDir");
+
         RecurrenceProgramProcess recurrenceProgramProcess = new RecurrenceProgramProcess();
 
-        recurrenceProgramProcess.reproduceProgramProcess("C:\\Users\\yuegu\\AppData\\Roaming\\CppMonitor","E:\\tmp\\testOutput","E:\\tmp\\tmpDir");
+        recurrenceProgramProcess.reproduceProgramProcess(sourceDir,targetDir,tmpDir);
     }
 }
